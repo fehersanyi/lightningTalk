436ea47c9142a6767cb6b266dd7782caa1ea96d3
fehersanyi <papahadacow@gmail.com>
2018-04-11 22:38:02 +0200

    second step

--- forPractice/willISmoke.js
+++ forPractice/willISmoke.js
@@ -2,30 +2,33 @@ let timeIWokeUp = 800;
 let missedDays = 0;
 let timesIWasLate = 0;
 
-function myFunction() {
+function amILucky() {
+	if (Math.random() > 0.5) {
+		//do i care?
+		if((missedDays >= 2 || timesIWasLate >= 4) || timeIWokeUp > 800) {
+			return 'no';
+			//or do i?
+		} else if(missedDays < 2 || timesIWasLate < 4) {
+			return 'yes';
+			// or do i?
+		} else {
+			return 'yes';
+		}
+		//am i late?
+	} 
+	// no
+	else {
+		return 'yes'
+	}
+}
+
+function didISleepTooMuch() {
 	//too much?
 	if (timeIWokeUp > 730) {
 	//yes
 		//did i miss it?
 		//am i lucky?
 		//no
-		if (Math.random() > 0.5) {
-			//do i care?
-			if((missedDays >= 2 || timesIWasLate >= 4) || timeIWokeUp > 800) {
-				return 'no';
-				//or do i?
-			} else if(missedDays < 2 || timesIWasLate < 4) {
-				return 'yes';
-				// or do i?
-			} else {
-				return 'yes';
-			}
-			//am i late?
-		} 
-		// no
-		else {
-			return 'yes'
-		}
 	}
 	// no i dont
 	else {
