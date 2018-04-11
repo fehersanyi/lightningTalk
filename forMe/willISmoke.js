7fbf09f7a39aec14ba692383c748d2b970878503
fehersanyi <papahadacow@gmail.com>
2018-04-11 19:53:52 +0200

    for me it is ready

---
+++ forMe/willISmoke.js
@@ -0,0 +1,58 @@
+const timeIWokeUp = 720;
+const missedDays = 0;
+const timesIWasLate = 0;
+
+function willISmoke() {
+	if (doICare() === true || amILate() === true) {
+		return 'no, i need to hurry';
+	} else if (doICare() === false) {
+		return 'yes i will';
+	} else {
+		return 'yes i will';
+	}
+};
+
+function doICare() {
+	if (missedDays >= 2 || timesIWasLate >= 4) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+function amILate() {
+	if (timeIWokeUp < 800) {
+		return false;
+	} else {
+		didIMissTheTroli()
+	}
+};
+
+function didIMissTheTroli() {
+	if (didISleepTooMuch()) {
+		if (amILucky() === false) {
+			return true;
+		}
+	} else {
+		return false;
+	}
+};
+
+
+function amILucky() {
+	if (Math.random() > 0.5) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function didISleepTooMuch() {
+	if (timeIWokeUp > 730) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+console.log(willISmoke());
